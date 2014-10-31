721cd8bd41ca21965b3b591ed11b26544c0d7efc
John de Wasseige <johndewasseige@me.com>
2014-10-31 18:35:13 +0100

    ameliorations

--- outil.m
+++ outil.m
@@ -3,13 +3,13 @@ function out = outil(T, m_NH3)
 % a partir de methane.
 % 
 % in - T     = temperature en KELVIN reacteur
-%    - m_NH3 = masse en tonnes d'ammoniac
+%    - m_NH3 = masse d'ammoniac en tonnes par jour
 % 
-% out- m_CH4 = masse en tonnes de methane
-%    - m_H2O = masse en tonnes d'eau
-%    - m_O2  = masse en tonnes d'oxygene
-%    - m_N2  = masse en tonnes de nitrogen
-%    - m_Ar  = masse en tonnes d'argon
+% out- m_CH4 = masse de methane en tonnes par jour
+%    - m_H2O = masse d'eau en tonnes par jour
+%    - m_O2  = masse d'oxygene en tonnes par jour
+%    - m_N2  = masse de nitrogen en tonnes par jour
+%    - m_Ar  = masse d'argon en tonnes par jour
 %    - nombre de tubes
 
 
@@ -25,10 +25,6 @@ else
 end
 cp_h2o = [30.09200 6.832514 6.793435 -2.534480 ...
     0.082139 -250.8810 223.3967 -241.8264] ;
-cp_n2  = [19.50583 19.88705 -8.598535 1.369784 ...
-    0.527601 -4.935202 212.3900 0] ;
-cp_nh3 = [19.99563 49.77119 -15.37599	1.921168 ...
-    0.189174 -53.30667 203.8591 -45.89806] ;
 cp_co  = [25.56759 6.096130 4.054656 -2.671301 ...
     0.131021 -118.0089 227.3665 -110.5271] ;
 cp_o2  = [30.03235 8.772972 -3.988133 0.788313 ...
@@ -75,7 +71,7 @@ K_r2 = exp(-dG_r2/(R*T)) ;
 % resolution des equations pour trouver le 
 % nombre de moles de CH4 et de H2O 
 
-syms ksi1 ksi2 n_CH4 n_H2O positive 
+syms ksi1 ksi2 n_CH4 n_H2O positive
 
 % equilibre des pressions reaction 1 reformage primaire
 eq1= K_r1 == (p_tot^2 *(ksi1 - ksi2)*(3*ksi1 + ksi2)^3)...
@@ -97,23 +93,23 @@ eq4= 4*ksi1 == (9/221)*m_NH3*1e6 ;
     solve(eq1, eq2, eq3, eq4, n_CH4, n_H2O, ksi1, ksi2);
 
 % masses
-m_CH4= 16*n_CH4/1e6 ;
-m_H2O= 18*n_H2O/1e6 ;
+m_CH4= 16*double(n_CH4)/1e6 ;
+m_H2O= 18*double(n_H2O)/1e6 ;
 m_O2 = 32*((7/884)* m_NH3) ; 
 m_N2 = 28*((1/34)* m_NH3) ;
 m_Ar = 40*((1/2652)* m_NH3) ;
 
 
 disp(['Quantite de CH4 en tonnes par jour : ', ...
-    num2str(roundn(double(m_CH4),-2))])
+    num2str(roundn(m_CH4,-2))])
 disp(['Quantite de H2O en tonnes par jour : ', ... 
-    num2str(roundn(double(m_H2O),-2))])
+    num2str(roundn(m_H2O,-2))])
 disp(['Quantite de O2 en tonnes par jour : ', ...
-    num2str(roundn(double(m_O2),-2))])
+    num2str(roundn(m_O2,-2))])
 disp(['Quantite de N2 en tonnes par jour : ', ...
-    num2str(roundn(double(m_N2),-2))])
+    num2str(roundn(m_N2,-2))])
 disp(['Quantite de Ar en tonnes par jour : ', ...
-    num2str(roundn(double(m_Ar),-2))])
+    num2str(roundn(m_Ar,-2))])
 
 % out = [m_CH4 m_H2O m_O2 m_N2 m_Ar] ;
 
