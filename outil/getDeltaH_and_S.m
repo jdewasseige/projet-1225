250180b083e0298b3c7ddc54371af16f3953210d
John de Wasseige <johndewasseige@me.com>
2014-11-19 16:07:02 +0100

    improve outil de gestion

--- outil/getDeltaH_and_S.m
+++ outil/getDeltaH_and_S.m
@@ -13,18 +13,22 @@ t = T/1000;
 calcH = [t t^2/2 t^3/3 t^4/4 -1/t 1 0 0] ; % en kilo Joules
 calcS = [log(t) t t^2/2 t^3/3 -1/(2*t^2) 0 1 0] ;
 
+dH.h2 = cp.h2*calcH' * 1000 ;
+dS.h2 = cp.h2*calcS' ;
+dH.h2o= cp.h2o*calcH' * 1000 ;
+dS.h2o= cp.h2o*calcS' ;
 dH.co = cp.co*calcH' * 1000 ;
 dS.co = cp.co*calcS' ;
 dH.o2 = cp.o2*calcH'  *1000 ;
 dS.o2 = cp.o2*calcS' ;
-dH.h2 = cp.h2*calcH' * 1000 ;
-dS.h2 = cp.h2*calcS' ;
-dH.ch4= cp.ch4*calcH' * 1000 ;
-dS.ch4= cp.ch4*calcS' ;
 dH.co2= cp.co2*calcH' * 1000 ;
 dS.co2= cp.co2*calcS' ;
-dH.h2o= cp.h2o*calcH' * 1000 ;
-dS.h2o= cp.h2o*calcS' ;
+dH.ch4= cp.ch4*calcH' * 1000 ;
+dS.ch4= cp.ch4*calcS' ;
+dH.n2 = cp.n2*calcH' * 1000 ;
+dS.n2 = cp.n2*calcS' ;
+dH.nh3= cp.nh3*calcH' * 1000 ;
+dS.nh3= cp.nh3*calcS' ;
 
 out = [dH dS];
 
--- outil/main.m
+++ outil/main.m
@@ -1,4 +1,4 @@
-function out = outil(T, m_NH3)
+function out = main(T, m_NH3)
 % Outil de gestion du plant de formation d'ammoniac
 % a partir de methane.
 % 
@@ -33,7 +33,7 @@ dG_r2 = dH.r2 - T*dS.r2 ;
 
 % constantes d'eq K
 R = 8.3144621 ;
-p_tot = 28e5 ;
+p_tot = 26e5 ;
 p_st = 1e5 ;
 
 K_r1 = exp(-dG_r1/(R*T)) ;
@@ -41,43 +41,29 @@ K_r2 = exp(-dG_r2/(R*T)) ;
 
 % resolution des equations pour trouver le 
 % nombre de moles de CH4 et de H2O 
-
-syms ksi1 ksi2 n_CH4 n_H2O positive
-
-% equilibre des pressions reaction 1 reformage primaire
-eq1= K_r1 == (p_tot^2 *(ksi1 - ksi2)*(3*ksi1 + ksi2)^3)...
-    /(p_st^2 *(n_CH4 + n_H2O + 2*ksi1)^2 *(n_CH4 - ksi1)* ...
-    (n_H2O - ksi1 - ksi2)) ;
-
-% equilibre des pressions reaction 2 (WGS) reformage primaire
-eq2= K_r2 == (ksi2*(3*ksi1 + ksi2)) ...
-    /((ksi1 - ksi2)*(n_H2O - ksi1 - ksi2)) ;
-
-% donnee a l'entree du reformage secondaire
-eq3= n_CH4 - ksi1 == (7/442)*m_NH3*1e6 ;
-
-% donnee a la sortie du reformage secondaire
-eq4= 4*ksi1 == (9/221)*m_NH3*1e6 ;
-
-% resoudre le systeme de 4 equations a 4 inconnues 
-[n_CH4,n_H2O,ksi1,ksi2] = ... 
-    solve(eq1, eq2, eq3, eq4, n_CH4, n_H2O, ksi1, ksi2);
+moles = solveG(m_NH3,p_tot,K_r1,K_r2) ;
+n_CH4 = moles(1) ;
+n_H2O = moles(2) ;
+ksi1  = moles(3) ;
+ksi2  = moles(4) ;
 
 % Masses molaires
 M = getMolarMasses();
+
 % masses
-m_CH4= M.ch4*double(n_CH4)/1e6 ;
-m_H2O= M.h2o*double(n_H2O)/1e6 ;
+m_CH4= M.ch4*n_CH4/1e6 ;
+m_H2O= M.h2o*n_H2O/1e6 ;
 m_O2 = M.o2*((7/884)* m_NH3) ; 
 m_N2 = M.n2*((1/34)* m_NH3) ;
 m_Ar = M.ar*((1/2652)* m_NH3) ;
 
 
-fprintf('Quantite de CH4 (REF 1) en tonnes par jour : %.2f \n', m_CH4) ;
-fprintf('Quantite de H20 (REF 1) en tonnes par jour : %.2f \n', m_H2O) ;
-fprintf('Quantite de O2 (REF 1) en tonnes par jour : %.2f \n', m_O2) ;
-fprintf('Quantite de N2 (REF 1) en tonnes par jour : %.2f \n', m_N2) ;
-fprintf('Quantite de Ar (REF 1) en tonnes par jour : %.2f \n', m_Ar) ;
+fprintf('\n In REF1 - Quantite de CH4 en tonnes par jour : %.2f \n', m_CH4) ;
+fprintf('In REF1 - Quantite de H20 en tonnes par jour : %.2f \n', m_H2O) ;
+
+fprintf('In REF2 - Quantite de O2 (REF 2) en tonnes par jour : %.2f \n', m_O2) ;
+fprintf('In REF2 - Quantite de N2 (REF 2) en tonnes par jour : %.2f \n', m_N2) ;
+fprintf('In REF2 - Quantite de Ar (REF 2) en tonnes par jour : %.2f \n', m_Ar) ;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -85,20 +71,17 @@ fprintf('Quantite de Ar (REF 1) en tonnes par jour : %.2f \n', m_Ar) ;
 
 tubes = getTubesNumber(n_CH4,n_H2O,T) ;
 
-fprintf('Nombre de tubes : %d \n', ceil(double(tubes))) ;
+fprintf('\nNombre de tubes : %d \n \n', ceil(double(tubes))) ;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Bilan energetique
-
-ksi1 = double(ksi1);
-ksi2 = double(ksi2);
 
 oven_masses = getHovenMasses(ksi1,ksi2,dH.r1,dH.r2);
 m_CH4_four = oven_masses(1) ;
 m_O2_four  = oven_masses(2) ;
 
 fprintf('Quantite de CH4 (FOUR) en tonnes par jour : %.2f \n', m_CH4_four) ;
-fprintf('Quantite de O2 (FOUR) en tonnes par jour : %.2f \n', m_O2_four) ;
+fprintf('Quantite de O2 (FOUR) en tonnes par jour : %.2f \n\n', m_O2_four) ;
 
 end
 
