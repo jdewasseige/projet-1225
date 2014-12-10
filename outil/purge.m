12004b3e9f2832adcf0143c84202c757f9bc9d2f
David <david.dispas@gmx.fr>
2014-12-11 00:50:17 +0100

    purge fonctionnelle (alleluia!) :+1:

--- outil/purge.m
+++ outil/purge.m
@@ -1,37 +1,37 @@
-function out = purge(T, m_NH3, T_reac, p_reac)
+function out = purge(m_NH3, T, p)
 
-%On obtient les différents flux molaires.
-n_N2_in   = (1/34)*m_NH3*1000;
-n_H2_in   = 3*n_N2_in;
-n_Ar_in   = (1/2652)*m_NH3*1000;
-n_in      = n_N2_in+n_H2_in+n_Ar_in;
-n_NH3_out = m_NH3/17;
+%On obtient les différents flux molaires journaliers.
+n_NH3_out = m_NH3/17.0305;
+n_N2_in = n_NH3_out/2;
+n_H2_in = 3*n_N2_in;
+n_Ar_in = (1/2652)*m_NH3*10^6;
+n_in = n_N2_in+n_H2_in+n_Ar_in;
 
-%On cherche K (à 750K)
+%On cherche K (à température T)
 R=8.3145;
-H_and_S=getDeltaH_and_S(T_reac);
+H_and_S=getDeltaH_and_S(T);
 dH=H_and_S(1);
 dS=H_and_S(2);
 dH=2*dH.nh3-dH.n2-3*dH.h2;
 dS=2*dS.nh3-dS.n2-3*dS.h2;
-dG=dH-T*dS+R*T_reac*log(p_reac); %ATTENTION on utilise la loi des gaz parfaits pour la contribution de la pression (très approximative)
-K= exp(-dG/(R*T_reac));
+dG=dH-T*dS+R*T*log(p); %ATTENTION on utilise la loi des gaz parfaits pour la contribution de la pression (très approximative)
+K= exp(-dG/(R*T));
 
-%On cherche xsi (uniquement pour le circuit "in"!!!) grâce à K
-syms xsi_s;
-xsi=solve(((2*xsi_s)^2)*((n_in-2*xsi_s)^2)/(27*(n_N2_in-xsi_s)^4)-K/p_reac, xsi_s); %Ce système a plusieurs solutions!
-xsi=double(xsi(3)); %On prend la solution qui a du sens
+%On cherche xi (uniquement pour le circuit "in"!!!) grâce à K
+syms xi_s;
+xi=solve( K == ((2*xi_s)^2) * ((n_in-2*xi_s)^2) * p^-2 / (27*(n_N2_in-xi_s)^4) , xi_s); %Ce système a plusieurs solutions!
+xi=double(xi(2)); %On prend la solution qui a du sens
 
 %On peut obtenir les deux valeurs suivantes grâce à nos bilans du départ
-n_purge=n_in-2*xsi-n_NH3_out;
+n_purge=n_in-2*xi-n_NH3_out;
 A_purge=n_Ar_in/n_purge;
 
 %On utilise K pour la réaction totale et on obtient la masse totale dans le
 %circuit de recyclage.
 syms n_N2_out_s;
-n_N2_out=solve(((((n_NH3_out)^2)*((4*n_N2_out_s+n_Ar_in-A_purge*n_NH3_out)/(1-A_purge))^2)/(27*(n_N2_out_s)^4))-K/p_reac, n_N2_out_s);
+n_N2_out=solve((((n_NH3_out)^2) * ((4*n_N2_out_s+n_NH3_out-(A_purge*n_NH3_out)/(1-A_purge))^2) / (27*p^2*(n_N2_out_s)^4))-K, n_N2_out_s)
 n_N2_out=double(n_N2_out(1));
-n_out=(4*n_N2_out+n_Ar_in-A_purge*n_NH3_out)/(1-A_purge);
+n_out=sqrt(K*(p^2)*27*n_N2_out^4/(n_NH3_out^2))
 n_rec=n_out-n_NH3_out;
 x=abs(n_purge/n_rec) %La fraction recherchée
 
@@ -39,5 +39,3 @@ m_rec=((n_rec-(A_purge*n_rec))/4)*(28+6)+A_purge*n_rec*40
 m_purge=m_rec*x
 
 end
-
-
