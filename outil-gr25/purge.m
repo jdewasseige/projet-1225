d9d9bfda109bafcd5316b27b199639acd56a302f
John de Wasseige <johndewasseige@me.com>
2014-12-17 16:00:02 +0100

    Revert "main rapport"
    This reverts commit 966e2d4866d8764e786be0696a0e3e4d3e6f0ce4.

--- outil-gr25/purge.m
+++ outil-gr25/purge.m
@@ -1,6 +1,6 @@
 function [x, eff] = purge(m_NH3, T, p)
 
-%On obtient les diffï¿½rents flux molaires journaliers..
+%On obtient les différents flux molaires journaliers..
 n_NH3_out_th = m_NH3*10^6/17.0305;
 n_N2_in = n_NH3_out_th/2;
 n_H2_in = 3*n_N2_in;
@@ -9,41 +9,41 @@ n_in = n_N2_in+n_H2_in+n_Ar_in;
 
 M = getMolarMasses();
 
-%On cherche K (ï¿½ tempï¿½rature T)
+%On cherche K (à température T)
 R=8.3145;
 H_and_S=getDeltaH_and_S(T);
 dH=H_and_S(1);
 dS=H_and_S(2);
 dH=dH.nh3-dH.n2/2-3*dH.h2/2;
 dS=dS.nh3-dS.n2/2-3*dS.h2/2;
-dG=dH-T*dS+R*T*log(p); %ATTENTION on utilise la loi des gaz parfaits pour la contribution de la pression (trï¿½s approximative)
+dG=dH-T*dS+R*T*log(p); %ATTENTION on utilise la loi des gaz parfaits pour la contribution de la pression (très approximative)
 K= exp(-dG/(R*T));
 
-%On cherche xi (uniquement pour le circuit "in"!!!) grï¿½ce ï¿½ K
+%On cherche xi (uniquement pour le circuit "in"!!!) grâce à K
 
 syms xi_s;
 
-xi_in=solve(((2*xi_s)^2) * ((n_in-2*xi_s)^2) / (27*p^2*(n_N2_in-xi_s)^4) - K , xi_s); %Ce systï¿½me a plusieurs solutions!
-xi_in=double(xi_in(2)); %On prend la solution qui a du sens
+xi_in=solve(((2*xi_s)^2) * ((n_in-2*xi_s)^2) / (27*p^2*(n_N2_in-xi_s)^4) - K , xi_s); %Ce système a plusieurs solutions!
+xi_in=double(xi(2)); %On prend la solution qui a du sens
 
 
-%On peut obtenir les deux valeurs suivantes grï¿½ce ï¿½ nos bilans du dï¿½part
+%On peut obtenir les deux valeurs suivantes grâce à nos bilans du départ
 
-n_purge=n_in-4*xi_in
-xi_rec=(n_in-n_purge-4*xi_in)/4
-n_NH3_out=2*xi_in+2*xi_rec
+n_purge=n_in-2*xi-n_NH3_out;
+A_purge=n_Ar_in/n_purge ;
 
-A_purge=n_Ar_in/n_purge 
-
-%On utilise K pour la rï¿½action totale et on obtient la masse totale dans le
+%On utilise K pour la réaction totale et on obtient la masse totale dans le
 %circuit de recyclage.
 
+xi_rec=(n_in-n_purge-4*xi)/4;
+n_NH3_out=2*xi_in+2*xi_rec;
+
 syms n_N2_out_rec_s;
-n_N2_out_rec=solve((((2*xi_rec)^2) * ((4*n_N2_out_rec_s/(1-A_purge)-2*xi_rec)^2) / (27*p^2*(n_N2_out_rec_s-xi_rec)^4))-K, n_N2_out_rec_s)
-n_N2_out=n_N2_out_rec(1)+n_N2_in-xi_in;
+n_N2_out_rec=solve((((2*xi_rec)^2) * (((4*n_N2_out_rec_s/(1-A_purge)-2*xi_rec)^2) / (27*p^2*(n_N2_out_rec_s-xi_rec)^4))-K, n_N2_out_rec_s);
+n_N2_out=double(n_N2_out_rec(1))+n_N2_in-xi_in;
 n_rec=4*n_N2_out_rec/(1-A_purge);
 
-x=abs(n_purge/n_rec);%La fraction recherchï¿½e
+x=abs(n_purge/n_rec);%La fraction recherchée
 
 eff=n_NH3_out_th/n_NH3_out;
 
