00a7038424145b4e8ace989b4127ed80e1a1672d
John de Wasseige <johndewasseige@me.com>
2015-01-12 22:36:22 +0100

    add pdf

--- .root.mk
+++ .root.mk
@@ -4,7 +4,7 @@ all: $(NAME).tex
 	$(COMPILER) -pdf -pdflatex="pdflatex -shell-escape -enable-write18" \
 		-use-make -auxdir=file -outdir=file $(NAME) ;
 	shopt -s extglob ;
-	mv file/*.pdf file/pdf
+	cp file/*.pdf file/pdf
 
 init: 
 	mkdir file ;
