726a296f84c36f1c34d425ee788d5db65fe479b4
John de Wasseige <johndewasseige@me.com>
2015-02-27 14:56:01 +0100

    much easier way of keeping only pdfs

--- .root.mk
+++ .root.mk
@@ -4,8 +4,4 @@ all: $(NAME).tex
 	$(COMPILER) -pdf -pdflatex="pdflatex -shell-escape -enable-write18" \
 		-use-make -auxdir=file -outdir=file $(NAME) ;
 	shopt -s extglob ;
-	cp file/*.pdf file/pdf
-
-init: 
-	mkdir file ;
-	mkdir file/pdf 
+	cp file/*.pdf ./
